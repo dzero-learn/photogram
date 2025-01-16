1bbb24fe96fbd291a3920b6c62fe2b5abb6ccf4b
dayeoung <rhdek228@gmail.com>
2025-01-16 09:02:36 +0900

    [나] facebook 로그인 연동 개발 중

---
+++ src/main/java/com/cos/photogramstart/service/CustomOAuth2UserService.java
@@ -0,0 +1,16 @@
+package com.cos.photogramstart.service;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.printf("oAuth2User.getAttributes()");
+
+        return super.loadUser(userRequest);
+    }
+}
